f1c84aaba098111dffdc3adfff3c7ba5d6ad3633
gioac <gioac@DESKTOP-ORABO2D>
2019-12-20 16:41:32 +0100

    jbutton ok

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -6,13 +6,16 @@ public class Application {
 public static void main(String args[])
 	{
 		JFrame win;
-		win = new JFrame("Esempio√di√JComboBox");
-		String lista[]=new String[10];
-		for(int i=0;i<lista.length;i++)
-		lista[i]="Elemento√numero√"+i;
-		JComboBox cBox=new JComboBox(lista);
+		win = new JFrame("Esempio di button");
 		Container c = win.getContentPane();
-		c.add(cBox);
+		
+		//JLabel xField=new JLabel("x = ");	//etichetta + compo per scrivere
+		//JTextField xField=new JTextField(5); //per costruire un campo di testo
+		
+		JButton moveButton=new JButton("Move");
+		//JButton moveButton=new JButton(new ImageIcon("hand.gif"));
+		
+		c.add(moveButton);
 		win.setSize(200,200);
 		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		win.setVisible(true);
