c1bac3343d1f61b9515d3db8d5837f1c83099d18
gioac <gioac@DESKTOP-ORABO2D>
2019-12-20 19:36:34 +0100

    19.36

--- src/gui/Application1.java
+++ src/gui/Application1.java
@@ -2,14 +2,16 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
-class MyFrame1 extends JFrame
+
+//classe frame creata utilizzando un Flow Layout
+class MyFrameFL extends JFrame
 {
 	JButton uno=new JButton("Uno");
 	JButton due=new JButton("Due");
 	JButton tre=new JButton("Tre");
 	JButton quattro=new JButton("Quattro");
 	JButton cinque = new JButton("Cinque");
-	public MyFrame1()
+	public MyFrameFL()
 	{
 		super("Flow Layout");
 		Container c = this.getContentPane();
@@ -23,10 +25,28 @@ class MyFrame1 extends JFrame
 		setVisible(true);
 	}
 }
+
+//classe frame creata utilizzando un Grid Layout
+class MyFrameGL extends JFrame
+{
+	public MyFrameGL()
+	{
+		super("Grid Layout");
+		Container c = this.getContentPane();
+		c.setLayout(new GridLayout(4,4));
+		for(int i = 0; i<15; i++)
+			c.add(new JButton(String.valueOf(i)));
+		setSize(300,300);
+		setVisible(true);
+	}
+}
+
 public class Application1
 {
 	public static void main(String args[])
 	{
-		MyFrame1 MyFrame = new MyFrame1();
+		MyFrameFL MyFrameFL = new MyFrameFL();
+		MyFrameGL MyFrameGL = new MyFrameGL();
+		MyFrameGL.setLocation(200,200);
 	}
 }
