5fbd381e48cf36b8159762451c7bcc8476713c6b
gioac <gioac@DESKTOP-ORABO2D>
2019-12-20 12:28:15 +0100

    jframe

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -4,11 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 public class Application {
 	public static void main(String args[])
-		{
+	{
 		JFrame win;
-		win = new JFrame("Prima√finestra");
+		win = new JFrame("Prima finestra");
 		Container c = win.getContentPane();
-		c.add(new JLabel("Buona√Lezione"));
+		c.add(new JLabel("Buona Lezione"));
 		win.setSize(200,200);
 		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		win.setVisible(true);
