6086f79f279b31bd086425fd96fec090d44cca6c
gioac <gioac@DESKTOP-ORABO2D>
2019-12-20 16:27:41 +0100

    application ok

--- src/gui/Application.java
+++ src/gui/Application.java
@@ -11,7 +11,7 @@ public class Application {
 		win.setSize(300,300);
 		//win.setLocation(600,200);
 		//win.setResizable(false);	//non permette resize finestra all'utente
-		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//comandi consentiti
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//ci sono altri comandi consentiti
 		win.setVisible(true); //permette di visualizzare e di nascondere la finestra
 
 		Container c = win.getContentPane();
