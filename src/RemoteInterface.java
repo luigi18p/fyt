e6bce0fc01066cfc6e65af2a1b54cb849e41cc3d
Luigi Palermo <40658753+luigi18p@users.noreply.github.com>
2019-12-20 11:33:01 +0100

    Rename RemoteInterface.java to src/RemoteInterface.java

--- src/RemoteInterface.java
+++ src/RemoteInterface.java
@@ -1,7 +1,7 @@
 import java.rmi.Remote; 
 import java.rmi.RemoteException;  
 
-// Crea interfaccia remota "RemoteInterface" che sar· implementata da "InterfaceImplementation"
+// Crea interfaccia remota "RemoteInterface" che sar√° implementata da "InterfaceImplementation"
 // Dichiara i Metodi
 
 public interface RemoteInterface extends Remote {  
