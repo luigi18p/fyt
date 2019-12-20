dd65d68db730f10e5987af711c456258a379c335
gioac <gioac@DESKTOP-ORABO2D>
2019-12-20 11:56:42 +0100

    application caricata

---
+++ src/gui/Application.java
@@ -0,0 +1,25 @@
+package gui;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Application
+{
+	public static void main(String args[])
+	{
+		//STRUTTURA:	FRAME(finestra)->CONTAINER->
+		JFrame win;
+		win = new JFrame("Prima finestra");
+		win.setSize(300,300);
+		//win.setLocation(600,200);
+		//win.setResizable(false);	//non permette resize finestra all'utente
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//comandi consentiti
+		win.setVisible(true); //permette di visualizzare e di nascondere la finestra
+
+		Container c = win.getContentPane();
+		Container c1 = win.getContentPane();
+		c.add(new JLabel("Buona Lezione"));
+		
+	}
+}
