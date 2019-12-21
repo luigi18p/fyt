cba68a94a856ae93b208dbb63bb7ba5c639c3512
gioac <gioac@DESKTOP-ORABO2D>
2019-12-21 09:52:53 +0100

    ok checkBox

--- src/gui/Application1.java
+++ src/gui/Application1.java
@@ -1,6 +1,9 @@
 package gui;
 
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.event.*;
 import java.awt.*;
 
 //classe frame creata utilizzando un Flow Layout
@@ -41,6 +44,39 @@ class MyFrameGL extends JFrame
 	}
 }
 
+class RadioDemo1 extends JFrame {
+    //========================================================== constructor
+    RadioDemo1() {
+        //... Create the buttons.
+        JRadioButton yesButton   = new JRadioButton("Yes", true);
+        JRadioButton noButton    = new JRadioButton("No");
+        JRadioButton maybeButton = new JRadioButton("Maybe");
+
+        //... Create a button group and add the buttons.
+        ButtonGroup bgroup = new ButtonGroup();
+        bgroup.add(yesButton);
+        bgroup.add(noButton);
+        bgroup.add(maybeButton);
+        
+        //... Arrange buttons vertically in a panel
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new GridLayout(3, 1));
+        radioPanel.add(yesButton);
+        radioPanel.add(noButton);
+        radioPanel.add(maybeButton);
+        
+        //... Add a titled border to the button panel.
+        radioPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Married?"));
+
+        //... Set window attributes.
+        setContentPane(radioPanel);  // Button panel is only content.
+        pack();                      // Layout window.
+        
+        setSize(300,300);
+		setVisible(true);
+    }
+}
+		
 public class Application1
 {
 	public static void main(String args[])
@@ -48,5 +84,8 @@ public class Application1
 		MyFrameFL MyFrameFL = new MyFrameFL();
 		MyFrameGL MyFrameGL = new MyFrameGL();
 		MyFrameGL.setLocation(200,200);
+		RadioDemo1 RadioDemo1 = new RadioDemo1();
+		RadioDemo1.setTitle("CheckBox");
+		RadioDemo1.setLocation(500,200);
 	}
 }
