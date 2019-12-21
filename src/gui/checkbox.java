69fc7ec67183ec4a645f586ea2f8e50809d85036
gioac <gioac@DESKTOP-ORABO2D>
2019-12-21 09:39:52 +0100

    jradio non corretto

--- src/gui/checkbox.java
+++ src/gui/checkbox.java
@@ -8,23 +8,33 @@ public class checkbox {
 			JFrame win;
 			win = new JFrame("Esempio di button");
 			Container c = win.getContentPane();
+			c.setLayout(new FlowLayout());
 			
-			JCheckBox check1=new JCheckBox("JCheck");
+			JCheckBox check1=new JCheckBox("JCheck1");
+			JCheckBox check2=new JCheckBox("JCheck2");
+			JCheckBox check3=new JCheckBox("JCheck3");
+			JCheckBox check4=new JCheckBox("JCheck4");
+			JCheckBox check5=new JCheckBox("JCheck5");
 			//JButton moveButton=new JButton(new ImageIcon("hand.gif"));
 			c.add(check1);
+			c.add(check2);
+			c.add(check3);
+			c.add(check4);
+			c.add(check5);
 			
 			win.setSize(200,200);
 			win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			win.setVisible(true);
 			
-			
+			//______________________________________________________________________JRadioButton
 			JFrame win2;
 			win2 = new JFrame("Esempio di JRadioButton");
 			Container c2 = win2.getContentPane();
+
 			JRadioButton radioButton1=new JRadioButton("R1");
 			JRadioButton radioButton2=new JRadioButton("R2");
 			JRadioButton radioButton3=new JRadioButton("R3");
-			ButtonGroup group = new ButtonGroup();
+			ButtonGroup group = new ButtonGroup();//aggiungendo il group posso selezionarne solo 1 alla volta e selezionandone uno si deseleziona un altro
 			c2.add(radioButton1);
 			group.add(radioButton2);
 			group.add(radioButton3);
@@ -34,6 +44,7 @@ public class checkbox {
 			win2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			win2.setVisible(true);
 			
+			//______________________________________________________________________JComboBox
 			JFrame win3;
 			win3 = new JFrame("Esempio di JComboBox");
 			String lista[]=new String[10];
@@ -41,6 +52,7 @@ public class checkbox {
 				lista[i]="Elemento numero "+i;
 			JComboBox cBox=new JComboBox(lista);
 			Container c3 = win3.getContentPane();
+			c3.setLayout(new FlowLayout());
 			c3.add(cBox);
 			win3.setSize(200,200);
 			win3.setLocation(400, 0);
